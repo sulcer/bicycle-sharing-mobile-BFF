01f94079cb00dd093a7ec0e42c7e1d10cbfb9a8a
sulcer <gregor.sulcer@student.um.si>
2024-05-07 12:32:07 +0200

    feat: jwt auth

--- server.js
+++ server.js
@@ -14,6 +14,49 @@ app.use(helmet());
 app.use(morgan("combined"));
 app.use(express.json());
 
+// jwt authentication
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const user = { name: username };
+
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15m' });
+    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+
+    res.json({ accessToken: accessToken, refreshToken: refreshToken });
+});
+
+
+let refreshTokens = [];
+app.post('/token', (req, res) => {
+    const refreshToken = req.body.token;
+    if (refreshToken == null) return res.sendStatus(401);
+    if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+        const accessToken = jwt.sign({ name: user.name }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15m' });
+        res.json({ accessToken: accessToken });
+    });
+});
+
+app.delete('/logout', (req, res) => {
+    refreshTokens = refreshTokens.filter(token => token !== req.body.token);
+    res.sendStatus(204);
+});
+
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (token == null) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+}
+
 const rateLimit = process.env.RATELIMIT;
 const rateLimitInterval = process.env.RATELIMIT_WINDOW;
 
